f33b208ec3b9e897ecbe96aed0079602c9580d01
Trafire <antoinewood@gmail.com>
2020-08-15 02:14:00 -0400

    created employer model

--- furniture_guys/settings.py
+++ furniture_guys/settings.py
@@ -16,15 +16,15 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 
-if os.getenv("GAE_INSTANCE"): #checks if production environment
+if os.getenv("GAE_INSTANCE"):  # checks if production environment
     DEBUG = False
 else:
     import secrets
+
     DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -32,17 +32,17 @@ SECRET_KEY = '(e0c=b$9xkch67c*1+v3ny_b^l_f+y738_(vn!4s!qos@8urk4'
 
 ALLOWED_HOSTS = ['(127.0.0.1', 'localhost', 'afg-jobs.appspot.com']
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'jobs',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jobs',
+
 ]
 
 MIDDLEWARE = [
@@ -76,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'furniture_guys.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -87,22 +86,16 @@ WSGI_APPLICATION = 'furniture_guys.wsgi.application'
     }
 }'''
 
-
-
-
-
-
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'HOST': os.environ['DB_HOST'],
-            'PORT': os.environ['DB_PORT'],
-            'NAME': os.environ['DB_NAME'],
-            'USER': os.environ['DB_USER'],
-            'PASSWORD': os.environ['DB_PASSWORD'],
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
     }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -122,7 +115,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -135,7 +127,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
